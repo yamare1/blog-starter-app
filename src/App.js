2c511c63ea75a225c38d7610b0ccb99091cac438
Yonathan Amare <yamare1@lion.lmu.edu>
2021-11-01 22:53:15 -0700

    adding files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { signInWithPopup, GoogleAuthProvider, signOut } from "@firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "./firebaseConfig";
 import Nav from "./Nav";
 import Article from "./Article";
 import { fetchTitles } from "./articleService";
+
+
+function SignIn() {
+  function signInWithGoogle() {
+    signInWithPopup(auth, new GoogleAuthProvider());
+  }
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function TodoList() {
+  return <div>TodoList goes here</div>;
+}
+
+function App() {
+  const [user] = useAuthState(auth);
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <section>{user ? <TodoList /> : <SignIn />}</section>
+    </div>
+  );
+}
+
 
 export default function App() {
   useEffect(() => {
@@ -11,12 +37,14 @@ export default function App() {
     );
   }, []);
 
+  const [user] = useAuthState(auth);
   const [titles, setTitles] = useState([]);
   const [articleId, setArticleId] = useState(null);
   return (
     <div className="App">
-      <Nav titles={titles} setArticleId={setArticleId} />
-      <Article articleId={articleId} />
+      <header>{!user ? <SignIn /> : <SignOut />}</header>
+      {user && <Nav titles={titles} setArticleId={setArticleId} />
+      {user && <Article articleId={articleId} />
     </div>
   );
 }
